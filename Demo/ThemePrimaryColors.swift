c6759e235de4d27e4ec1372ddf69ff449387f77d
Octree <fouljz@gmail.com>
2018-08-13 22:07:11 +0800

    📱 demo

---
+++ Demo/ThemePrimaryColors.swift
@@ -0,0 +1,31 @@
+//
+//  ThemePrimaryColors.swift
+//  uLens
+//
+//  Created by Octree on 2018/8/12.
+//  Copyright © 2018年 Octree. All rights reserved.
+//
+
+import Foundation
+
+let ThemePrimaryColors: [UInt32] = [
+    0xf44336,
+    0xe91e63,
+    0x9c27b0,
+    0x673ab7,
+    0x3f51b5,
+    0x2196f3,
+    0x03a9f4,
+    0x00bcd4,
+    0x009688,
+    0x4caf50,
+    0x8bc34a,
+    0xcddc39,
+    0xffeb3b,
+    0xffc107,
+    0xff9800,
+    0xff5722,
+    0x795548,
+    0x9e9e9e,
+    0x607d8b
+]
--- Demo/ViewController.swift
+++ Demo/ViewController.swift
@@ -1,19 +1,103 @@
 //
 //  ViewController.swift
-//  Demo
+//  ThemeKit
 //
 //  Created by Octree on 2018/8/13.
 //  Copyright © 2018年 Octree. All rights reserved.
 //
 
 import UIKit
+import ThemeKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var satButtons: [UIButton]!
+    
+    
+    @IBOutlet weak var primaryBg: UIView!
+    @IBOutlet weak var secondaryBg: UIView!
+    @IBOutlet weak var positiveBg: UIView!
+    
+    @IBOutlet var colorButtons: [UIButton]!
+    
+    @IBOutlet weak var lb1: UILabel!
+    @IBOutlet weak var lb2: UILabel!
+    @IBOutlet weak var lb3: UILabel!
+    @IBOutlet weak var lbOver: UILabel!
+    @IBOutlet weak var positiveButton: UIButton!
+    
+    
+    var primaryColor = UIColor(hex6: ThemePrimaryColors[0])
+    var style = ThemeManager.Style.light
+    var color = UIColor(hex6: ThemePrimaryColors[0])
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        renderButtons()
+        bindTheme()
+        updateSatButtons()
     }
+    
+    func bindTheme() {
+        
+        primaryBg.tk.backgroundColor = \Theme.background.primary
+        secondaryBg.tk.backgroundColor = \Theme.background.secondary
+        positiveBg.tk.backgroundColor = \Theme.base.positive
 
-
+        lb1.tk.textColor = \Theme.text.primary
+        lb2.tk.textColor = \Theme.text.secondary
+        lb3.tk.textColor = \Theme.text.tertiary
+        lbOver.tk.textColor = \Theme.text.overPositive
+        
+        positiveButton.tk.setTitleColor(\Theme.base.destructive, forState: .normal)
+        positiveButton.tk.setTitleColor(\Theme.base.darkPositive, forState: .highlighted)
+    }
+    
+    func renderButtons() {
+        
+        for (index,button) in colorButtons.enumerated() {
+            button.backgroundColor = UIColor(hex6: ThemePrimaryColors[index])
+        }
+    }
+    
+    func updateSatButtons() {
+        
+        let colors = self.primaryColor.suggestColors
+        for (index, button) in satButtons.enumerated() {
+            button.backgroundColor = colors[index]
+        }
+    }
+    
+    @IBAction func satButtonTapped(_ sender: UIButton) {
+        
+        color = sender.backgroundColor!
+        ThemeManager.shared.changeTheme(with: color, style: style)
+    }
+    
+    @IBAction func selectColor(_ sender: UIButton) {
+        
+        primaryColor = sender.backgroundColor!
+        color = primaryColor
+        updateSatButtons()
+        ThemeManager.shared.changeTheme(with: color, style: style)
+    }
+    
+    @IBAction func selectLight(_ sender: UIButton) {
+        
+        style = .light
+        ThemeManager.shared.changeTheme(with: color, style: style)
+    }
+    @IBAction func selectDark(_ sender: UIButton) {
+        
+        style = .dark
+        ThemeManager.shared.changeTheme(with: color, style: style)
+    }
+    
+    @IBAction func selectNight(_ sender: UIButton) {
+        
+        style = .night
+        ThemeManager.shared.changeTheme(with: color, style: style)
+    }
 }
+
 
