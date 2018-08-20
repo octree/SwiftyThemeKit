713652a1b2f9432ef582264395f8a130f3d559aa
Octree <fouljz@gmail.com>
2018-08-20 17:47:04 +0800

    🔥 just remove some useless code

--- ThemeKit/UIColor+Extension/Color+Theme.swift
+++ ThemeKit/UIColor+Extension/Color+Theme.swift
@@ -9,30 +9,6 @@
 
 import UIKit
 
-/*
- darkText: {
- primary: "rgba(0, 0, 0, 0.87)",
- secondary: "rgba(0, 0, 0, 0.54)",
- disabled: "rgba(0, 0, 0, 0.38)",
- dividers: "rgba(0, 0, 0, 0.12)"
- },
- lightText: {
- primary: "rgba(255, 255, 255, 1)",
- secondary: "rgba(255, 255, 255, 0.7)",
- disabled: "rgba(255, 255, 255, 0.5)",
- dividers: "rgba(255, 255, 255, 0.12)"
- },
- darkIcons: {
- active: "rgba(0, 0, 0, 0.54)",
- inactive: "rgba(0, 0, 0, 0.38)"
- },
- lightIcons: {
- active: "rgba(255, 255, 255, 1)",
- inactive: "rgba(255, 255, 255, 0.5)"
- },
- */
-
-
 public extension UIColor {
     
     
