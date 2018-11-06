9c462f79d7bc9d726403ede606292633629d8c10
Octree <fouljz@gmail.com>
2018-11-06 15:32:27 +0800

    📱 update demo

--- Demo/Controller/Theme/ViewController.swift
+++ Demo/Controller/Theme/ViewController.swift
@@ -12,25 +12,12 @@ import ThemeKit
 class ViewController: UIViewController {
     
     @IBOutlet var satButtons: [UIButton]!
-    
-    
-    @IBOutlet weak var primaryBg: UIView!
-    @IBOutlet weak var secondaryBg: UIView!
-    @IBOutlet weak var positiveBg: UIView!
-    
     @IBOutlet var colorButtons: [UIButton]!
     
-    @IBOutlet weak var lb1: UILabel!
-    @IBOutlet weak var lb2: UILabel!
-    @IBOutlet weak var lb3: UILabel!
-    @IBOutlet weak var lbOver: UILabel!
-    @IBOutlet weak var positiveButton: UIButton!
-    
-    @IBOutlet weak var imageView: UIImageView!
-    
-    var primaryColor = UIColor(hex6: ThemePrimaryColors[0])
-    var style = ThemeManager.Style.light
-    var color = UIColor(hex6: ThemePrimaryColors[0])
+    var primaryColor: UIColor {
+        
+        return ThemeManager.shared.primaryColor
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,25 +28,12 @@ class ViewController: UIViewController {
     
     func bindTheme() {
         
-        primaryBg.tk.backgroundColor = \Theme.background.primary
-        secondaryBg.tk.backgroundColor = \Theme.background.secondary
-        positiveBg.tk.backgroundColor = \Theme.base.positive
-
-        lb1.tk.textColor = \Theme.text.primary
-        lb2.tk.textColor = \Theme.text.secondary
-        lb3.tk.textColor = \Theme.text.tertiary
-        lbOver.tk.textColor = \Theme.text.overPositive
-        
-        positiveButton.tk.setTitleColor(\Theme.base.destructive, forState: .normal)
-        positiveButton.tk.setTitleColor(\Theme.base.darkPositive, forState: .highlighted)
         
         navigationController?.navigationBar.tk.barTintColor = \Theme.background.bar
         navigationController?.navigationBar.isTranslucent = false
         navigationController?.navigationBar.tk.tintColor = \Theme.base.positive
         navigationController?.navigationBar.tk.titleColor = \Theme.text.primary
-        
-        imageView.image = UIImage(named: "Check")?.withRenderingMode(.alwaysTemplate)
-        imageView.tk.tintColor = \Theme.base.positive
+        view.tk.backgroundColor = \Theme.background.primary
     }
     
     func renderButtons() {
@@ -80,36 +54,30 @@ class ViewController: UIViewController {
     
     @IBAction func satButtonTapped(_ sender: UIButton) {
         
-        color = sender.backgroundColor!
-        ThemeManager.shared.changeTheme(with: color, style: style)
+        ThemeManager.shared.changeTheme(with: sender.backgroundColor!, style: ThemeManager.shared.style)
     }
     
     @IBAction func selectColor(_ sender: UIButton) {
         
-        primaryColor = sender.backgroundColor!
-        color = primaryColor
+        ThemeManager.shared.changeTheme(with: sender.backgroundColor!, style: ThemeManager.shared.style)
         updateSatButtons()
-        ThemeManager.shared.changeTheme(with: color, style: style)
     }
     
     @IBAction func selectLight(_ sender: UIButton) {
         
-        style = .light
         let color = ThemeManager.shared.primaryColor
-        ThemeManager.shared.changeTheme(with: color, style: style)
+        ThemeManager.shared.changeTheme(with: color, style: .light)
     }
     @IBAction func selectDark(_ sender: UIButton) {
         
-        style = .dark
         let color = ThemeManager.shared.primaryColor
-        ThemeManager.shared.changeTheme(with: color, style: style)
+        ThemeManager.shared.changeTheme(with: color, style: .dark)
     }
     
     @IBAction func selectNight(_ sender: UIButton) {
         
-        style = .night
         let color = ThemeManager.shared.primaryColor
-        ThemeManager.shared.changeTheme(with: color, style: style)
+        ThemeManager.shared.changeTheme(with: color, style: .night)
     }
 }
 
